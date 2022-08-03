e343bcca2b2262d66a82750af3caf4bea8fbfe4c
Muhammad Latif Harkat <68332606+Latip176@users.noreply.github.com>
2022-08-03 14:08:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var userAgent = window.navigator.userAgent;
 (async () => {
    var ip = (await fetch("https://api.ipify.org/?format=json").then(r => r.json())).ip;
       document.getElementById("ip").innerHTML = ip;
-   })();
+})();
 document.getElementById("UserAgent").innerHTML = userAgent;
